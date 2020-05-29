e2f4b8ea96ba6b431179971ab67587bdb65ab23b
tshi <timshi2013@gmail.com>
2020-05-29 18:09:14 +1200

    add step defs for search course by single criteria

---
+++ src/test/java/SearchCourseStepDef.java
@@ -0,0 +1,20 @@
+import cucumber.api.java8.En;
+
+/**
+ * @author Tim Shi
+ * @version 1.0
+ */
+public class SearchCourseStepDef implements En {
+    public SearchCourseStepDef() {
+        Given("^I login as a student$", () -> {
+        });
+        Given("^I click search course button from Student Dashboard page$", () -> {
+        });
+        When("^I search by \"([^\"]*)\"$", (String arg0) -> {
+        });
+        And("^I click search button$", () -> {
+        });
+        Then("^the search result should be displayed on Search Result page$", () -> {
+        });
+    }
+}
