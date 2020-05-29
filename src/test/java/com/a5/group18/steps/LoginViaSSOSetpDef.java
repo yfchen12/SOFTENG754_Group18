af6a9693a6c3dcac78310f2f1e0016a70ef464e3
tshi <timshi2013@gmail.com>
2020-05-29 14:23:55 +1200

    add test scenario for course admin login via sso

---
+++ src/test/java/com/a5/group18/steps/LoginViaSSOSetpDef.java
@@ -0,0 +1,24 @@
+package com.a5.group18.steps;
+
+import cucumber.api.java8.En;
+
+/**
+ * @author Tim Shi
+ * @version 1.0
+ */
+public class LoginViaSSOSetpDef implements En {
+    public LoginViaSSOSetpDef() {
+        Given("^open UoA course enrollment website$", () -> {
+        });
+        When("^I click login via SSO button$", () -> {
+        });
+        Then("^I should be redirected to UoA SSO page$", () -> {
+        });
+        When("^I enter my user credentials$", () -> {
+        });
+        And("^I click login button$", () -> {
+        });
+        Then("^I should be login to dashboard page sccessful$", () -> {
+        });
+    }
+}
