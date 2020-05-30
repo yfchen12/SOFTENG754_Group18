02028266c8940b8db70294ebbb29b95a2af9e877
tshi <timshi2013@gmail.com>
2020-05-30 18:42:24 +1200

    add step def of add course to cart from search result test

---
+++ src/test/java/com/a5/group18/steps/CartManagementStepDef.java
@@ -0,0 +1,18 @@
+package com.a5.group18.steps;
+
+import cucumber.api.java8.En;
+
+/**
+ * @author Tim Shi
+ * @version 1.0
+ */
+public class CartManagementStepDef implements En {
+    public CartManagementStepDef() {
+        Given("^I found my desired course from search result$", () -> {
+        });
+        When("^I click Add To Cart button$", () -> {
+        });
+        Then("^the course should be added to cart successfully$", () -> {
+        });
+    }
+}
