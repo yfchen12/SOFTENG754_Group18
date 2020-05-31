1fed5207d2e64c5fc7b8f2005868ff25091ea1f3
Wulei Gui <wilbert.gui.nz@gmail.com>
2020-05-31 18:06:57 +1200

    feature/update course information: test steps

---
+++ src/test/java/com/a5/group18/steps/UpdateCourseInformationStepdefs.java
@@ -0,0 +1,18 @@
+package com.a5.group18.steps;
+
+import cucumber.api.java8.En;
+
+public class UpdateCourseInformationStepdefs implements En {
+    public UpdateCourseInformationStepdefs() {
+        When("^I click edit course button from admin Dashboard page$", () -> {
+        });
+        Then("^I should be redirected to courses list page$", () -> {
+        });
+        When("^I press edit button in the list$", () -> {
+        });
+        Then("^I should be redirected to courses edit page$", () -> {
+        });
+        And("^Course information is successfully updated$", () -> {
+        });
+    }
+}
