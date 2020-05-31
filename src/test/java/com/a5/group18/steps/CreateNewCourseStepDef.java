812522acf0afdcba4d2e8232fbccc64aa233a69e
Wulei Gui <wilbert.gui.nz@gmail.com>
2020-05-31 17:02:36 +1200

    feature/create new course: steps file

---
+++ src/test/java/com/a5/group18/steps/CreateNewCourseStepDef.java
@@ -0,0 +1,18 @@
+package com.a5.group18.steps;
+
+import cucumber.api.java8.En;
+
+public class CreateNewCourseStepDef implements En {
+    public CreateNewCourseStepDef() {
+        When("^I click create new course button from admin Dashboard page$", () -> {
+        });
+        Then("^I should be redirected to create course information page$", () -> {
+        });
+        When("^I finished all the mandatory information$", () -> {
+        });
+        And("^I click submit button$", () -> {
+        });
+        Then("^I should be redirected to course information page successful$", () -> {
+        });
+    }
+}
