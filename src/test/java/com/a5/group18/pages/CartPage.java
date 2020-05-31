b28cc811bcb9904cf3e5030b9d15f7e04eed370f
tshi <timshi2013@gmail.com>
2020-05-31 16:11:59 +1200

    add automation and application code for add course to cart from search result

---
+++ src/test/java/com/a5/group18/pages/CartPage.java
@@ -0,0 +1,24 @@
+package com.a5.group18.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * @author Tim Shi
+ * @version 1.0
+ */
+public class CartPage {
+    private WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "page_title")
+    public WebElement strTitle;
+
+}
