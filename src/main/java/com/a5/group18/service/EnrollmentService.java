3bdb0ba02a23e9c867683dd0de5f07b15269c208
tshi <timshi2013@gmail.com>
2020-05-31 17:16:06 +1200

    refactor

--- src/main/java/com/a5/group18/service/EnrollmentService.java
+++ src/main/java/com/a5/group18/service/EnrollmentService.java
@@ -5,24 +5,24 @@ import org.springframework.stereotype.Service;
 import com.a5.group18.enumerated.EnrollmentStatus;
 import com.a5.group18.pojo.Enrollment;
 import com.a5.group18.pojo.Student;
+
 import java.util.Arrays;
 import java.util.List;
 
 
+@Service
+public class EnrollmentService {
+    public List<Enrollment> findEnrollment() {
+        CourseService courseService = new CourseService();
+        List<Course> course = courseService.findAll();
+        Student John = new Student();
+        John.setUpi("jsm123");
+        Enrollment enrollment701 = new Enrollment(John, course.get(0), EnrollmentStatus.ENROLLED);
+        Enrollment enrollment702 = new Enrollment(John, course.get(1), EnrollmentStatus.WAITLISTED);
+        Enrollment enrollment703 = new Enrollment(John, course.get(2), EnrollmentStatus.CONCESSION_PENDING);
 
-    @Service
-    public class EnrollmentService {
-        public List<Enrollment> findEnrollment(){
-            CourseService courseService = new CourseService();
-            List<Course> course = courseService.findAll();
-            Student John = new Student();
-            John.setUpi("jsm123");
-            Enrollment enrollment701 = new Enrollment(John,course.get(0),EnrollmentStatus.ENROLLED);
-            Enrollment enrollment702 = new Enrollment(John,course.get(1),EnrollmentStatus.WAITLISTED);
-            Enrollment enrollment703 = new Enrollment(John,course.get(2),EnrollmentStatus.CONCESSION_PENDING);
-
-            return Arrays.asList(enrollment701,enrollment702,enrollment703);
-        }
+        return Arrays.asList(enrollment701, enrollment702, enrollment703);
     }
+}
 
 
