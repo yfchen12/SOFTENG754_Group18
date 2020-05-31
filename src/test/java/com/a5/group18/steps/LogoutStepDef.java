28d3d55f3070d59b7c5b7d13184af1b43a4843a3
tshi <timshi2013@gmail.com>
2020-05-31 17:36:52 +1200

    add automation and application code for logout test

--- src/test/java/com/a5/group18/steps/LogoutStepDef.java
+++ src/test/java/com/a5/group18/steps/LogoutStepDef.java
@@ -1,16 +1,28 @@
 package com.a5.group18.steps;
 
+import com.a5.group18.pages.IndexPage;
+import com.a5.group18.pages.StdDashboardPage;
 import cucumber.api.java8.En;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author Tim Shi
  * @version 1.0
  */
 public class LogoutStepDef implements En {
+
+    @Autowired
+    UIGlobalState state;
+    private StdDashboardPage stdDashPage;
+
     public LogoutStepDef() {
         When("^I click Logout button$", () -> {
+            stdDashPage = new StdDashboardPage(state.driver);
+            stdDashPage.btnLogout.click();
         });
         Then("^I should be able to logout successfully$", () -> {
+            Assert.assertEquals("UoA Enrollment System", state.driver.getTitle());
         });
     }
 }
