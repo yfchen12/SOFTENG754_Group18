e3a3f4b0cfe760e53a1b4762c0cb99c47e09a354
tshi <timshi2013@gmail.com>
2020-05-31 17:28:39 +1200

    add step def for logout test

---
+++ src/test/java/com/a5/group18/steps/LogoutStepDef.java
@@ -0,0 +1,16 @@
+package com.a5.group18.steps;
+
+import cucumber.api.java8.En;
+
+/**
+ * @author Tim Shi
+ * @version 1.0
+ */
+public class LogoutStepDef implements En {
+    public LogoutStepDef() {
+        When("^I click Logout button$", () -> {
+        });
+        Then("^I should be able to logout successfully$", () -> {
+        });
+    }
+}
