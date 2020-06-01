305d9d22258f720cde4d01c1427a03572a9b2787
yfchen12 <chenyeefei90@gmail.com>
2020-06-01 17:46:24 +1200

    add automation and application code for scenario students are notified when successsfully drop a course

--- src/test/java/com/a5/group18/steps/StudentManageEnrollmentStepDef.java
+++ src/test/java/com/a5/group18/steps/StudentManageEnrollmentStepDef.java
@@ -3,8 +3,12 @@ package com.a5.group18.steps;
 import com.a5.group18.pages.MyEnrollmentPage;
 import cucumber.api.java8.En;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class StudentManageEnrollmentStepDef implements En {
 
@@ -31,11 +35,21 @@ public class StudentManageEnrollmentStepDef implements En {
             Assertions.assertFalse(myEnrollmentPage.modalMsg.getText().contains("allen"));
         });
 
+
         When("^I click the drop button of course SOFTENG702$", () -> {
+            WebElement dropbtn702 = state.driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr[2]/td[4]/button"));
+            state.wait.until(ExpectedConditions.elementToBeClickable(dropbtn702));
+            dropbtn702.click();
         });
         And("^I click the confirm button in the modal window$", () -> {
+            WebElement confirmBtn = state.driver.findElement(By.id("confirmDrop"));
+            state.wait.until(ExpectedConditions.visibilityOf(confirmBtn));
+            confirmBtn.click();
         });
         Then("^I should be able to notify the course is dropped successfully$", () -> {
+            state.wait.until(ExpectedConditions.alertIsPresent());
+            String msg =  state.driver.switchTo().alert().getText();
+            Assertions.assertTrue(msg.contains("Succeed"));
         });
     }
 }
