d04dfac2644073a60616216366a0faff541abd2e
Wulei Gui <wilbert.gui.nz@gmail.com>
2020-06-03 11:32:20 +1200

    feature/outline: cart redirect -step definition

---
+++ src/test/java/com/a5/group18/steps/StudentCheckCourseInformationStepdefs.java
@@ -0,0 +1,19 @@
+package com.a5.group18.steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StudentCheckCourseInformationStepdefs {
+    @And("I am in enrollment cart page")
+    public void iAmInEnrollmentCartPage() {
+    }
+
+    @When("I click <CourseNumber> in enroll cart list")
+    public void iClickCourseNumberInEnrollCartList() {
+    }
+
+    @Then("I should be redirected to <CourseNumber> information page")
+    public void iShouldBeRedirectedToCourseNumberInformationPage() {
+    }
+}
