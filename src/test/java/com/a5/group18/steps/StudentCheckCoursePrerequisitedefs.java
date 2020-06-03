a5584aa351ada8c5fd4e42fc639d45ac3fcc752d
Wulei Gui <wilbert.gui.nz@gmail.com>
2020-06-03 13:17:21 +1200

    feature/prerequisite: add step definition

---
+++ src/test/java/com/a5/group18/steps/StudentCheckCoursePrerequisitedefs.java
@@ -0,0 +1,12 @@
+package com.a5.group18.steps;
+
+import cucumber.api.java8.En;
+
+public class StudentCheckCoursePrerequisitedefs implements En {
+    public StudentCheckCoursePrerequisitedefs() {
+        Then("^I should be able to see the course information page$", () -> {
+        });
+        And("^I should be able to redirected to course information through prerequisite course name$", () -> {
+        });
+    }
+}
