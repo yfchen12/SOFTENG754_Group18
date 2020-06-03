5f7dc1ef66316266efcbcd9cded0db1d5372d27e
Wulei Gui <wilbert.gui.nz@gmail.com>
2020-06-03 14:09:03 +1200

    feature/coursevalue: step definition

---
+++ src/test/java/com/a5/group18/steps/StudentCheckCourseValueStepdefs.java
@@ -0,0 +1,12 @@
+package com.a5.group18.steps;
+
+import cucumber.api.java8.En;
+
+public class StudentCheckCourseValueStepdefs implements En {
+    public StudentCheckCourseValueStepdefs() {
+        Then("^I should be able to see the course value$", () -> {
+        });
+        And("^I should be able to see the <ValueType> and <LastChecktime> by <Company>$", () -> {
+        });
+    }
+}
