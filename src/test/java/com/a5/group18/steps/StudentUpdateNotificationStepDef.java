9b5f76b9856cf06479894ce3b946369636d4fdf8
Lucas H <61823872+jhsi517@users.noreply.github.com>
2020-06-04 00:16:46 +1200

    add step def for setting email as preferred notification method

---
+++ src/test/java/com/a5/group18/steps/StudentUpdateNotificationStepDef.java
@@ -0,0 +1,41 @@
+package com.a5.group18.steps;
+
+import com.a5.group18.pages.AdminDashboard;
+import com.a5.group18.pages.ManageConcessionPage;
+import com.a5.group18.pages.ProcessConcessionPage;
+import com.a5.group18.pages.StdDashboardPage;
+import cucumber.api.java8.En;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StudentUpdateNotificationStepDef implements En {
+
+    private StdDashboardPage studentDashPage;
+
+    @Autowired
+    private UIGlobalState state;
+    public StudentUpdateNotificationStepDef() {
+        When("^I click update notification button$", () -> {
+            state.wait.until(ExpectedConditions.titleIs("Student Dashboard"));
+            studentDashPage = new StdDashboardPage(state.driver);
+            studentDashPage.btnUpdateNotification.click();
+        });
+
+        And("^I select email as the preferred notification method$", () -> {
+            state.wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("preferredMethods")));
+            
+
+        });
+
+        And("^I click save button$", () -> {
+            state.wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("saveNotificationChanges")));
+            studentDashPage.btnSaveNotificationChanges.click();
+        });
+
+        Then("^I should see a green banner saying preference saved$", () -> {
+            Assert.assertTrue(state.driver.getPageSource().contains(notificationChangeSuccess));
+        });
+    }
+}
