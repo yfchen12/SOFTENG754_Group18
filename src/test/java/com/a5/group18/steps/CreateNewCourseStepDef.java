539312590dbaffa1071a618a10e8a3a5bc7ca6ac
Wulei Gui <wilbert.gui.nz@gmail.com>
2020-06-04 12:26:59 +1200

    Refactor pages. change add new course function to update page

--- src/test/java/com/a5/group18/steps/CreateNewCourseStepDef.java
+++ src/test/java/com/a5/group18/steps/CreateNewCourseStepDef.java
@@ -3,6 +3,7 @@ package com.a5.group18.steps;
 
 import com.a5.group18.pages.AddNewCoursePage;
 import com.a5.group18.pages.AdminDashboard;
+import com.a5.group18.pages.CourseManagePage;
 import cucumber.api.java8.En;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +11,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CreateNewCourseStepDef implements En {
     private AdminDashboard adminDashPage;
     private AddNewCoursePage addNewCoursePage;
+    private CourseManagePage courseManagePage;
     @Autowired
     private UIGlobalState state;
 
     public CreateNewCourseStepDef() {
 
-        When("^I click create new course button from admin Dashboard page$", () -> {
+        When("^I click create new course button from course manage page$", () -> {
             adminDashPage = new AdminDashboard(state.driver);
             addNewCoursePage = new AddNewCoursePage(state.driver);
-            adminDashPage.btnAddNewCourse.click();
+            adminDashPage.btnUpdateCourse.click();
+            state.wait.until(ExpectedConditions.titleIs("Course Manage"));
+            courseManagePage = new CourseManagePage(state.driver);
+            courseManagePage.btnAddNewCourse.click();
+
         });
         Then("^I should be redirected to create course information page$", () -> {
             state.wait.until(ExpectedConditions.titleIs("Add New Course"));
